91ca7bb99d015696f3ef0cba081eebc8bd6084fb
Tyler Knight <TylerKnight2197@gmail.com>
2021-10-01 20:14:47 -0400

    first commit

---
+++ src/main/java/com/coding_dojo/java_daikichi_routes/JavaDaikichiPathVariablesApplication.java
@@ -0,0 +1,34 @@
+package com.coding_dojo.java_daikichi_routes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+@RestController
+public class JavaDaikichiPathVariablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDaikichiPathVariablesApplication.class, args);
+	}
+	
+	@RequestMapping("/daikichi/travel/{city}")
+    public String showcity(@PathVariable ("city") String city) {
+            return "Congratulations! You will soon travel to" + city + "!";
+    }
+
+	
+	@RequestMapping("/daikichi/lotto/{number}")
+    public String todayfortune(@PathVariable ("number") int number) {
+        if (number % 2 == 0) {
+        	return "You will take a grand journey in the near future, but be weary of tempting offers";
+        }
+        else {
+        	return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends";
+        }
+    }
+
+}
---
+++ src/test/java/com/coding_dojo/java_daikichi_routes/JavaDaikichiPathVariablesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding_dojo.java_daikichi_routes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDaikichiPathVariablesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
